c9077a15c6de96a154f8d11ace84b535dcd9e2fc
陈景元 <2084470563@qq.com>
2022-10-04 15:14:34 +0800

    組件卡片

--- lowcode-components/src/components/management/Card/testCard.tsx
+++ lowcode-components/src/components/management/Card/testCard.tsx
@@ -1,7 +1,16 @@
 import Card from './index'
 import Image from '@/assets/1.jpg'
 function TestCard() {
-	return <Card image={Image} />
+	return (
+		<>
+			<Card
+				image={Image}
+				title='1231'
+				width={'20%'}
+				content='asdasdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+			/>
+		</>
+	)
 }
 
 export default TestCard
