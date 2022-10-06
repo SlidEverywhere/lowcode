ebc7f6dc1f0a035153c5b4275b8c751520aa12be
hannah <ttt123n2@outlook.com>
2022-10-06 12:25:17 +0800

    ‘登录'

--- lowcode-ui/src/api/index.ts
+++ lowcode-ui/src/api/index.ts
@@ -1,22 +1,26 @@
+import http from './request/http'
+
 const api = {
   // 获取用户信息
-  // getUserInfo: (data) => http('get', '', '/proxy/getUserInfo', data),
+  getUserInfo: (data) => http('get', '', '/auth/login', data)
+
+
   // 模拟从接口获取用户信息
-  getUserInfo () {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        console.log('got userInfo')
-        resolve({
-          data: {
-            userId: '666',
-            nickName: 'Neo',
-            accessIdList: ['10000', '10001', '10002']
-          },
-          errorCode: 0,
-        })
-      }, 200)
-    })
-  },
+  // getUserInfo () {
+  //   return new Promise((resolve) => {
+  //     setTimeout(() => {
+  //       console.log('got userInfo')
+  //       resolve({
+  //         data: {
+  //           userId: '666',
+  //           nickName: 'Neo',
+  //           accessIdList: ['10000', '10001', '10002']
+  //         },
+  //         errorCode: 0,
+  //       })
+  //     }, 200)
+  //   })
+  // },
 }
 
 export default api
